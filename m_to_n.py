a6faf625e552887382f1c1ab8caabb920847c44d
Aziz9022 <125199054+Aziz9022@users.noreply.github.com>
2023-02-21 20:50:42 +0530

    Add files via upload

---
+++ m_to_n.py
@@ -0,0 +1,6 @@
+m=int(input("enter any number: "))
+n=int(input("enter any number: "))
+if(m > n):
+	print("The second number is smaller than first , please enter a valid range")
+for i in range (m,n+1):
+    print(i)
